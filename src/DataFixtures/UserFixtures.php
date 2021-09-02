9af63eee188c76f01ade3a833dfb159ab58f13b9
Maryem Ben Massaoud <maryem.benmassoud@esprit.tn>
2021-09-02 16:57:44 +0100

    modif_integ

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\DataFixtures;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    /**
+     * UserFixtures constructor.
+     *
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     */
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 0;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     *
+     * @throws \Exception
+     */
+    public function load(ObjectManager $manager)
+    {
+        $usersList = [
+            [
+                'email' => 'tomek@test.pl',
+                'password' => 'password',
+                'role' => 'ROLE_ADMIN',
+                
+            ],
+
+        ];
+
+        foreach ($usersList as $userDetails) {
+            $user = new User();
+            $user->setPassword($this->passwordEncoder->encodePassword($user, $userDetails['password']))
+                    
+                    ->setEmail($userDetails['email'])
+               
+                    ->setRoles([$userDetails['role']]);
+                  
+
+            $this->addReference('user-'.$userDetails['password'], $user);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
